c9b510f55fcb3ed80e5990fb7a7c85721cde6971
MohaMed AshRaf <mohmmedashrraf@gmail.com>
2024-04-30 23:20:45 +0300

    .

--- src/app/Components/todo/todo.component.ts
+++ src/app/Components/todo/todo.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Task } from '../../Models/task';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
@@ -25,21 +25,21 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList }
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
-  todoForm! : FormGroup;
+  todoForm!: FormGroup;
   tasks: Task[] = [];
-  inprogress:Task[] = [];
-  done:Task[] = [];
+  inprogress: Task[] = [];
+  done: Task[] = [];
   isEditEnabled: boolean = false;
-  updatedIndex!:any;
+  updatedIndex!: any;
 
-  constructor(private fb:FormBuilder){}
+  constructor(private fb: FormBuilder) { }
   ngOnInit(): void {
     this.todoForm = this.fb.group({
-      item : ['', [Validators.required, Validators.minLength(3)]]
+      item: ['', [Validators.required, Validators.minLength(3)]]
     });
   }
 
-  addTask(){
+  addTask() {
     this.tasks.push({
       Title: this.todoForm.value.item,
       Completed: false,
@@ -49,27 +49,27 @@ export class TodoComponent {
   }
 
 
-  onEditTask(task:Task,taskId:number){ //take item from list and put it in input
+  onEditTask(task: Task, taskId: number) { //take item from list and put it in input
     this.isEditEnabled = true;
     this.updatedIndex = taskId;
     this.todoForm.controls['item'].setValue(task.Title);
   }
-  updateTask(){ //update item in input and appeat in list
+  updateTask() { //update item in input and appeat in list
     this.isEditEnabled = false;
-    this.tasks[this.updatedIndex].Title=this.todoForm.value.item;
-    this.tasks[this.updatedIndex].Completed=false;
+    this.tasks[this.updatedIndex].Title = this.todoForm.value.item;
+    this.tasks[this.updatedIndex].Completed = false;
     this.todoForm.reset();
     this.updatedIndex = -1;
   }
 
-deleteTask(taskId:number){
-    this.tasks.splice(taskId,1);
+  deleteTask(taskId: number) {
+    this.tasks.splice(taskId, 1);
   }
-  deleteInprogressTask(taskId:number){
-    this.inprogress.splice(taskId,1);
+  deleteInprogressTask(taskId: number) {
+    this.inprogress.splice(taskId, 1);
   }
-  deleteDoneTask(taskId:number){
-    this.done.splice(taskId,1);
+  deleteDoneTask(taskId: number) {
+    this.done.splice(taskId, 1);
   }
 
 
@@ -87,3 +87,34 @@ deleteTask(taskId:number){
   }
 
 }
+
+// <div
+//   cdkDropList
+//   #todoList1="cdkDropList" ريفرنس امسك بيه الليست
+//   [cdkDropListData]="tasks" الارااي اللي بتجيب منه الداتا
+//   [cdkDropListConnectedTo]="[todoList2, todoList3]" اقدر اشد منها العنصر وارميه في ليست 2 او 3
+//   class="example-list"
+//   (cdkDropListDropped)="drop($event)"
+// >
+//   @for (item of tasks; track $index) {
+//   <div class="example-box" cdkDrag>
+//     {{ item.Title }}
+//     <div class="box">
+//       <button
+//         mat-icon-button
+//         color="warn"
+//         (click)="deleteTask($index)"
+//       >
+//         <mat-icon>delete</mat-icon>
+//       </button>
+//       <button
+//         mat-icon-button
+//         color="primary"
+//         (click)="onEditTask(item, $index)"
+//       >
+//         <mat-icon>edit</mat-icon>
+//       </button>
+//     </div>
+//   </div>
+//   }
+// </div>
